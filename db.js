595f3ef82d172ff068759edd469f856cc308ee8b
Steven Kim <stevenn.kkim@gmail.com>
2023-12-21 09:32:47 -0800

    Update db.js

--- db.js
+++ db.js
@@ -3,8 +3,6 @@ import mongoose from 'mongoose';
 import updateAllProducts from './lib/update.js';
 dotenv.config();
 
-console.log(process.env.USERNAME, process.env.PASSWORD)
-
 mongoose.connect(`mongodb://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOST}:27017/${process.env.DB}`)
   .then(() => {
     console.log('Database connection successful');
